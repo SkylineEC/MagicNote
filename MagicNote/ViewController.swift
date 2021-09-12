809011d146644c5103b3ff2294a4be34e509cd2e
SkylineEC <e0724539@u.nus.edu>
2021-09-12 12:02:05 +0800

    Initial Commit

---
+++ MagicNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicNote
+//
+//  Created by Wang on 2021/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
